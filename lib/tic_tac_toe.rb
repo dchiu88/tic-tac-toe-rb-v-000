3f20d2edd60ea5b71285d88d48cbde76ec4cdf55
David Chiu <davidwchiu@gmail.com>
2018-02-19 20:39:33 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -46,13 +46,7 @@ def turn(board)
 end
 
 def turn_count(board)
-  turns = 0
-  board.each do |token|
-    if token == "X" || token == "O"
-      turns += 1
-    end
-  end
-  turns
+  board.count{|token| token == "X" || token == "O"}
 end
 
 def current_player(board)
