e11a11f95a8db2bd20e27baa77538e7788fe0280
David Chiu <davidwchiu@gmail.com>
2018-02-19 20:22:57 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -58,7 +58,12 @@ def turn_count(board)
 end
 
 def current_player(board)
-  turn_count(board) % 2 == 0 ? "X" : "O"
+  if turn_count(board)%2 ==0
+    current_player = "X"
+  else
+    current_player = "O"
+end
+return current_player
 end
 
 def won?(board)
