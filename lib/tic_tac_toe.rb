67195ea677de5483af15279b6367e6b0e7ef57f3
David Chiu <davidwchiu@gmail.com>
2018-02-19 20:36:45 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -30,9 +30,7 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
-  if index.between?(0,8) && !position_taken?(board, index)
-      true
-    end
+  index.between?(0,8) && !position_taken?(board, index)
 end
 
 def turn(board)
